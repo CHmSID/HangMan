7d5c06ec58ae46547acb0402e2a44ca89b5b69b2
Almarhoon <mbm.almarhoon@gmail.com>
2015-02-08 18:34:39 +0000

    Fixed some bugs

--- HangMan.java
+++ HangMan.java
@@ -19,18 +19,24 @@ class HangMan {
 		
 		Scanner in = new Scanner(System.in);
 		System.out.printf("%40s\n%s\n","HangMan","Keep in mind & and a space are also valid guesses");	//Name of the game.
-		System.out.printf("%s\n%s\n%s\n%s\n","Choose a category","1- word","2- country","3- name");
+		System.out.printf("%s\n%s\n%s\n%s\n%s\n","Choose a category","1- word","2- country","3- name","4-Random");
 		System.out.printf("%s","Please select a number : ");
 		int j = in.nextInt();
+		
+		if(j==4){							//pick a random category.
+			j = rand.nextInt(3) + 1;
+		}
+		
 		String guess =pick.getWord(j);		//getting a random word.
 		guess = guess.toLowerCase();		//convert it to lowercase.
 		
 		
 		for(int i=0;i<guess.length();i++){
-			word.add(0);		//let the ArrayList be 0.
+			word.add(0);					//let the ArrayList be 0.
 		}
 		
 		int num = 20; //the player only have 20 guesses.
+		int guessedLet=0;
 		while(num >0 ){
 		
 			for(int i=0;i<guess.length();i++){
@@ -41,30 +47,32 @@ class HangMan {
 					System.out.print(guess.charAt(i));
 				}
 			}
-			System.out.println();
+			
+			System.out.println();		//new line.
 			System.out.printf("%s","Guess the letter: ");
 			char let =in.next().charAt(0);
 			let=Character.toLowerCase(let);
 			for(int i=0;i<guess.length();i++){
 				if(let==guess.charAt(i)){
 					if(word.get(i)==1){		// check if the letter was guessed previously. 
-						//do nothing.
+						num++;
 					}
 					else{
 						word.set(i,1);
+						guessedLet++;
 					}
 				}
 				else{
 					;//do nothing for now
 				}
 			}
-			num--;
-			
+			num--;	
+			if(guessedLet==guess.length()){
+				num=0;
+			}
 		}
-
 		
 		System.out.println("The word was : "+guess);
-		
 	}
 }	
 
